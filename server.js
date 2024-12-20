efb9e8187addae3f092ba16371db0afac672ba81
soodala2003 <soodala78@gmail.com>
2024-12-19 19:38:03 -0500

    updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import db from "./db/conn.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-import Technology from "./models/Technology.js";
+//import Technology from "./models/Technology.js";
 import userRoutes from "./routes/user.route.js";
 import sourceRoutes from "./routes/source.route.js";
 import technologyRoutes from "./routes/technology.route.js";
@@ -14,15 +14,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-/* const corsOptions ={
+const corsOptions ={
   origin: "https://full-stack-mern-app-back-end.onrender.com",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
-}; */
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cors(corsOptions)); 
+app.use(cors(corsOptions)); 
 app.use(cookieParser());
 app.use(express.static("public"));
 
